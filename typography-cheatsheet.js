06c7205ed401d24e65c4bd361dadbafc78548778
wendigotech <wendigotech@gmail.com>
2022-11-01 00:24:42 +0100

    Update typography-cheatsheet.js
    remove to lowercase

--- typography-cheatsheet.js
+++ typography-cheatsheet.js
@@ -11,12 +11,6 @@ const seteventhandlers = () => {
 	let elements = document.querySelector('#elements');
 	elements.addEventListener('click', handleselect);
 }
-const handleselect = (ev) => {
-	if (['path','polygon','g'].indexOf(ev.target.nodeName.toLowerCase()) !== -1){
-		setbanner((information[ev.target.id]), ev.pageX, ev.pageY);
-		ev.preventDefault();
-	}
-};
 const init = () => {
 	seteventhandlers();
 }
